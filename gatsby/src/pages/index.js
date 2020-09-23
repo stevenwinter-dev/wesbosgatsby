d606f469411055eec1a5c943b9802aa7c23233b3
Steven Winter <stevenwinter.me@gmail.com>
2020-09-22 21:30:01 -0500

    Initial commit

---
+++ gatsby/src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Nav from '../components/Nav';
+import Layout from '../components/Layout';
+
+export default function HomePage() {
+  return (
+    <>
+      <p>Hey! I'm the home page!!!!!!!!!!!!</p>
+    </>
+  );
+}
